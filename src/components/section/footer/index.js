c0dfbbdb60cc917a882bb978aa116037182c436b
HizkiaPalar <Hizkiapalar294@gmail.com>
2024-11-16 01:20:06 +0800

    CV Done

--- src/components/section/footer/index.js
+++ src/components/section/footer/index.js
@@ -1,8 +1,9 @@
-
 import React, { useEffect, useState } from "react";
 import { getDatabase, ref, onValue } from "firebase/database";
+
 const Footer = () => {
   const [footer, setFooter] = useState({});
+
   useEffect(() => {
     const db = getDatabase();
     const footerRef = ref(db, "footer");
@@ -29,9 +30,7 @@ const Footer = () => {
                       <h5 className="title-left">Get in touch</h5>
                     </div>
                     <div className="more-info">
-                      <p className="lead">
-                        {footer.ty}
-                      </p>
+                      <p className="lead">{footer.ty}</p>
                       <ul className="list-ico">
                         <li>
                           <span className="ion-ios-location" /> {footer.location}
@@ -40,32 +39,46 @@ const Footer = () => {
                           <span className="ion-ios-telephone" /> {footer.phone}
                         </li>
                         <li>
-                          <span className="ion-email" />{" "}
-                          {footer.email}
+                          <span className="ion-email" /> {footer.email}
                         </li>
                       </ul>
                     </div>
                     <div className="socials">
                       <ul
                         className="d-flex justify-content-center"
-                        style={{ marginLeft: "18px" }} 
+                        style={{ marginLeft: "18px" }}
                       >
                         <li>
-                          <a href>
+                          <a
+                            href={footer.ig || "#"}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="social-link"
+                          >
                             <span className="ico-circle">
                               <i className="ion-social-instagram" />
                             </span>
                           </a>
                         </li>
                         <li>
-                          <a href>
+                          <a
+                            href={footer.linkedin || "#"}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="social-link"
+                          >
                             <span className="ico-circle">
                               <i className="ion-social-linkedin" />
                             </span>
                           </a>
                         </li>
                         <li>
-                          <a href>
+                          <a
+                            href={footer.github || "#"}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="social-link"
+                          >
                             <span className="ico-circle">
                               <i className="ion-social-github" />
                             </span>
